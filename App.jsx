be5c1cb7259f8045b873152d65c5f8e8b8bb4b58
pixelboogie <justamovie@yahoo.com>
2024-08-21 18:26:43 -0500

    Created Header

--- App.jsx
+++ App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-/**
- * Challenge: Build the Header component
- */
+import Header from './components/Header'
+import React from "react"
+
 export default function App() {
-  return <h1>Howdy world!</h1>
+  // return (<h1>Howdy world!</h1>)
+  return(
+    <Header />
+  )
 }
 
