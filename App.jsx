a3225aa713d97d8da5ef2d24b5af5a07be3e57c9
pixelboogie <justamovie@yahoo.com>
2024-08-21 19:11:49 -0500

    Added Memes data and log url of image

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Meme from './components/Meme'
 import React from "react"
 
 export default function App() {
-  // return (<h1>Howdy world!</h1>)
   return(
-    <Header />
+    <div>
+      <Header />
+      <Meme />
+    </div>
   )
 }
 
