ed3c93f1297afb5bb388193f35ba6a9ffe310794
pixelboogie <justamovie@yahoo.com>
2024-10-01 16:59:26 -0500

    Style tweaks and new image

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react"
+import { getImageURL } from "../image-util"
 
 export default function Header(){
     return(
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src={getImageURL("LaughingFace.png")} 
                 className="header--image"
             />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h2 className="header--title"> Meme Generator</h2>
+            <h4 className="header--project">HyperCanvas by PixelBoogie</h4>
         </header>
     )
 }
--- components/Meme.jsx
+++ components/Meme.jsx
@@ -58,7 +58,7 @@ export default function Meme(){
                     className="form--button"
                     onClick={getMemeImage}
                 >
-                    Get a new meme image 🖼
+                    Get a new meme image 🤣
                     </button>
             </div>
             <div className="meme">
